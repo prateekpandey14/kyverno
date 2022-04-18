bd30215c5506806c8f96f3a19f13f4a5ce6995fc
prateekpandey14 <prateek.pandey@nirmata.com>
2022-04-18 16:06:42 +0530

    fetch the dev tags from all the branch
    Signed-off-by: prateekpandey14 <prateek.pandey@nirmata.com>

--- scripts/create-e2e-infrastruture.sh
+++ scripts/create-e2e-infrastruture.sh
@@ -2,11 +2,12 @@
 set -e
 
 pwd=$(pwd)
-hash=$(git describe --match "[0-9].[0-9]-dev*")
-#
+
 ## Install Kind
 curl -Lo "$pwd"/kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
 chmod a+x "$pwd"/kind
+
+echo "kind load docker-image ${REPO}/kyverno:$IMAGE_TAG_DEV"
 
 ## Create Kind Cluster
 if [ -z "${KIND_IMAGE}" ]; then
@@ -15,13 +16,13 @@ else
     "$pwd"/kind create cluster --image="${KIND_IMAGE}"
 fi
 
-"$pwd"/kind load docker-image ghcr.io/kyverno/kyverno:"$hash"
-"$pwd"/kind load docker-image ghcr.io/kyverno/kyvernopre:"$hash"
+$pwd/kind load docker-image ${REPO}/kyverno:${IMAGE_TAG_DEV}
+$pwd/kind load docker-image ${REPO}/kyvernopre:${IMAGE_TAG_DEV}
 
 pwd=$(pwd)
 cd "$pwd"/config
 echo "Installing kustomize"
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
-kustomize edit set image ghcr.io/kyverno/kyverno:"$hash"
-kustomize edit set image ghcr.io/kyverno/kyvernopre:"$hash"
-kustomize build "$pwd"/config/ -o "$pwd"/config/install.yaml
+kustomize edit set image ${REPO}/kyverno:${IMAGE_TAG_DEV}
+kustomize edit set image ${REPO}/kyvernopre:${IMAGE_TAG_DEV}
+kustomize build $pwd/config/ -o $pwd/config/install.yaml
